79a1a4ae9ddd1b76e5ce4d1302b09b1aee715055
Hum Bao <49820991+Hum-Bao@users.noreply.github.com>
2024-05-26 16:57:52 -0700

    Update inventory.h

--- inventory.h
+++ inventory.h
@@ -19,17 +19,15 @@ class Inventory {
  public:
     // Default constructor
     Inventory();
-    // Adds movie to inventory using type as identifier
+
     bool AddMovie(const std::string& type, Movie* movie);
-    // Finds movie from type and title. Should this return a movie?
     bool FindMovie(const std::string& type, const std::string& title);
-    //Unsure how to incorporate Media here
-    bool addToStock(const std::string& type, Movie* movie, Media mediaType,
-                    int stock);
+    // Unsure what method does. Will implement later
+    bool addToStock(std::string type, Movie* movie, Media mediaType, int stock);
     void PrintInventory() const;
 
  private:
-    std::map<std::string, std::vector<Movie*>> inventory_;
+    map<std::string, std::vector<Movie*>> inventory_;
     using KeyValuePair = std::pair<std::string, std::vector<Movie*>>;
     void PrintHelper(const std::string& type) const;
 };
