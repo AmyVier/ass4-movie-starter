8cf377478586b3a8e66c8628f78f2a88cd871c35
Amy Vier <amyvier@icloud.com>
2024-05-27 09:37:30 -0700

    made changes

--- mediaFactory.cpp
+++
@@ -1,8 +0,0 @@
-#include <string>
-#include "mediaFactory.h"
-#include "dvd.h"
-
-static DVD MediaFactory::createDVD(int stock) {
-	DVD dvd = DVD(stock);
-	return dvd;
-}
--- movieFactory.h
+++ movieFactory.h
@@ -21,16 +21,19 @@ using namespace std;
 class MovieFactory
 {
 public:
-    static Movie* createComedy(char mediaType, int stock, string director, string title, int releaseYear) {
+    static Movie *createComedy(char mediaType, int stock, string director, string title, int releaseYear)
+    {
         return new Comedy(mediaType, stock, director, title, releaseYear);
     }
 
-    static Movie* createDrama(char mediaType, int stock, string director, string title, int releaseYear) {
+    static Movie *createDrama(char mediaType, int stock, string director, string title, int releaseYear)
+    {
         return new Drama(mediaType, stock, director, title, releaseYear);
     }
 
-    static Movie* createClassics(char mediaType, int stock, string director, string title,
-                          string majorActor, int releaseYear, int releaseMonth) {
+    static Movie *createClassics(char mediaType, int stock, string director, string title,
+                                 string majorActor, int releaseYear, int releaseMonth)
+    {
         return new Classics(mediaType, stock, director, title, majorActor, releaseYear, releaseMonth);
     }
 };
