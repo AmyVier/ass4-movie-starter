d71ab47c9a04836bbb8a7124330fd689ad1e0e7f
Chirisama <55990612+Chirisama@users.noreply.github.com>
2024-05-27 23:48:26 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -7,12 +7,9 @@ using namespace std;
 void test1()
 {
   MovieStore test = MovieStore();
-  test.readMovie("data5movies.txt");
-  std::cout << "movie input complete! " << endl;
+  test.readMovie("data4movies.txt");
   test.readCustomer("data4customers.txt");
-  std::cout << "customer input complete! " << endl;
   test.readCommand("data5commands.txt");
-  std::cout << "command input complete! " << endl;
 }
 
 int main()
